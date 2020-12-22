1003496733e55dc618aa2e221721494a02ec8948
vasilyevruslan <75842280+vasilyevruslan@users.noreply.github.com>
2020-12-22 12:58:55 +0200

    Add files via upload
    я ж прально скинул??

---
+++ руслан 1.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace руслан_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
